c8d33bb1a67346e691e62e5ca0346b4f21c58c02
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-10-07 11:54:57 +0530

    Create pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x = 2.0;
+    int n = 5;
+    double result = pow(x, n);
+    cout << x << "^" << n << " = " << result;
+    return 0;
+}
